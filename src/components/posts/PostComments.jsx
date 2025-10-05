611fbf9f831d3e9622b785fc519ec3ba3b6ae902
nayemurshuvo <nayemurshuvo@gmail.com>
2025-10-05 21:05:56 +0600

    Add like and comment functionality to posts

--- src/components/posts/PostComments.jsx
+++ src/components/posts/PostComments.jsx
@@ -1,17 +1,43 @@
 import { useState } from "react";
-import { useAvatar } from "../../hooks/useAvatar";
 import PostCommentList from "./PostCommentList";
+import { useAuth } from "../../hooks/useAuth";
+import useAxios from "../../hooks/useAxios";
 
 const PostComments = ({ post }) => {
-    const { avatarURL } = useAvatar(post);
+    const { auth } = useAuth();
     const [isShowAllComments, setIsShowAllComments] = useState(false);
+    const [comments, setComments] = useState(post?.comments || []);
+    const [comment, setComment] = useState("");
+    const { api } = useAxios();
+
+
+    const addComment = async (event) => {
+        const keyCode = event.keyCode;
+
+        // Enter key code is 13
+        if (keyCode === 13) {
+            try {
+                const response = await api.patch(
+                    `${import.meta.env.VITE_SERVER_BASE_URL}/posts/${post.id
+                    }/comment`,
+                    { comment }
+                );
+
+                if (response.status === 200) {
+                    setComments([...response.data.comments]);
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    };
 
     return (
         <div>
             <div className="flex-center mb-3 gap-2 lg:gap-4">
                 <img
                     className="max-w-7 max-h-7 rounded-full lg:max-h-[34px] lg:max-w-[34px]"
-                    src={avatarURL}
+                    src={`${import.meta.env.VITE_SERVER_BASE_URL}/${auth?.user?.avatar}`}
                     alt="avatar"
                 />
 
@@ -21,17 +47,20 @@ const PostComments = ({ post }) => {
                         className="h-8 w-full rounded-full bg-lighterDark px-4 text-xs focus:outline-none sm:h-[38px]"
                         name="post"
                         id="post"
+                        value={comment}
+                        onChange={(e) => setComment(e.target.value)}
+                        onKeyDown={(e) => addComment(e)}
                         placeholder="What's on your mind?"
                     />
                 </div>
             </div>
             <div className="mt-4">
                 <button className="text-gray-300 max-md:text-sm" onClick={() => setIsShowAllComments(!isShowAllComments)}>
-                    {isShowAllComments ? "Hide Comments" : `View all ${post?.comments?.length} comments`}
+                    {isShowAllComments ? "Hide Comments" : `View all comments`}
                 </button>
             </div>
 
-            {isShowAllComments && <PostCommentList comments={post?.comments} />}
+            {isShowAllComments && <PostCommentList comments={comments} />}
         </div>
     );
 };
