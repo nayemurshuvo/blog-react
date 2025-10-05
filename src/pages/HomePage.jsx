79adbbe5822cf27dec9afa532fe89df23ec28593
nayemurshuvo <nayemurshuvo@gmail.com>
2025-10-05 14:07:42 +0600

    Added post reducer and fetch posts on HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,52 @@
-import { Link } from "react-router-dom";
+import { useEffect, useReducer } from "react";
+import { actions } from "../actions";
+import PostList from "../components/posts/PostList";
+
+import { initialState, postReducer } from "../reducers/PostReducer";
+
+import useAxios from "../hooks/useAxios";
 
 const HomePage = () => {
+    const [state, dispatch] = useReducer(postReducer, initialState);
+    const { api } = useAxios();
+
+    useEffect(() => {
+        dispatch({ type: actions.post.DATA_FETCHING });
+
+        const fetchPost = async () => {
+            try {
+                const response = await api.get(
+                    `${import.meta.env.VITE_SERVER_BASE_URL}/posts`
+                );
+                if (response.status === 200) {
+                    dispatch({
+                        type: actions.post.DATA_FETCHED,
+                        data: response.data,
+                    });
+                }
+            } catch (error) {
+                console.error(error);
+                dispatch({
+                    type: actions.post.DATA_FETCH_ERROR,
+                    error: error.message,
+                });
+            }
+        };
+
+        fetchPost();
+    }, []);
+
+    if (state?.loading) {
+        return <div> Post Loading...</div>;
+    }
+
+    if (state?.error) {
+        return <div> Error in fetching posts {state?.error?.message}</div>;
+    }
+
     return (
         <div>
-            <p>This is HomePage.</p>
-            <Link to="/me">Go to Profile Page</Link>
+            <PostList posts={state?.posts} />
         </div>
     );
 };
